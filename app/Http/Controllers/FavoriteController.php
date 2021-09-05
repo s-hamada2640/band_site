613b9a249728a620885aff7e3f0c579faaf24767
morikawa0170 <mori7replicant10@gmail.com>
2021-09-05 22:48:29 +0900

    FavoriteControllerを作成

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Post;
+use App\User;
+use Auth;
 
 class FavoriteController extends Controller
 {
