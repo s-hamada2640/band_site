7e16538b876244dc9b45c913fd6d7631a97f8f5e
morikawa0170 <mori7replicant10@gmail.com>
2021-09-09 01:32:15 +0900

    ログイン後のルートを変更

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
@@ -23,8 +22,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-=======
 Route::post('posts/{post}/favorites', 'FavoriteController@store')->name('favorites');
 Route::post('posts/{post}/unfavorites', 'FavoriteController@destroy')->name('unfavorites');
 Route::post('posts/{post}/matching', 'FavoriteController@matching')->name('matching');
->>>>>>> 49a18c4fa398433f06d995bdf3f06d5beaf9718b
