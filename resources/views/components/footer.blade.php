c3ffe75d603e88efe34f0b852b792a548ea42345
morikawa0170 <mori7replicant10@gmail.com>
2021-09-10 01:25:12 +0900

    view header,footerテンプレート作成

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+   <small>Copyright&copy;Music×Search 2021</small>
+</footer>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,55 @@
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+   <div class="container">
+         <a class="navbar-brand" href="{{ url('/') }}">
+            <img src="{{asset('images/logo.png')}}" alt="Music×Search" height="50px">
+         </a>
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+         </button>
+
+         <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav mr-auto">
+
+            </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+               <!-- Authentication Links -->
+               @guest
+                     <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">
+                           <img src="{{asset('images/login.png')}}" style="height:22px;">
+                        </a>
+                     </li>
+                     @if (Route::has('register'))
+                        <li class="nav-item">
+                           <a class="nav-link" href="{{ route('register') }}">
+                              <img src="{{asset('images/sing-up.png')}}" style="height:22px;">
+                           </a>
+                        </li>
+                     @endif
+               @else
+                     <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                           ようこそ{{ Auth::user()->name }}さん <span class="caret"></span>
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                           <!-- <a class="dropdown-item" href="">{{ __('マイページ') }}</a> -->
+                           <a class="dropdown-item" href="{{ route('logout') }}"
+                              onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                 {{ __('ログアウト') }}
+                           </a>
+
+                           <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                 @csrf
+                           </form>
+                        </div>
+                     </li>
+               @endguest
+            </ul>
+         </div>
+   </div>
+</nav>
