9d34ee0b1214911c29e30e152fad3f71e37178b3
s-hamada2640 <hsat2640@gmail.com>
2021-09-18 21:31:16 +0900

    SearchController作成

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use App\User;
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    //
+    public function index() {
+        
+    }
+}
