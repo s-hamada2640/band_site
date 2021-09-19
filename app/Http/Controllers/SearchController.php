2c5aab1cfa888ec02f441881e92830bcae02cdb5
oohamaka <type_r_bathurst@me.com>
2021-09-19 17:28:11 +0900

    searchコントローラー作成

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -14,6 +14,27 @@ class SearchController extends Controller
         $keywords = Post::all();
 
         return view('search.search', compact('keywords'));
+
+        //都道府県で検索
+        $pref = $request->input('pref');
+        $genre = $request->input('genre');
+
+    //    public function show(Request $request) {
+    //        // メモが入力されているか確認
+    //        if($request->filled('memo')) {
+    //            $memo = $request->input('memo');
+    //        } else {
+    //            $memo = 'なし';
+    //        }
+    //        // ビューで使う配列データの作成
+    //        $profile = array(
+    //            'name' => $request->input('name'),
+    //            'age' => $request->input('age'),
+    //            'gender' => $request->input('gender'),
+    //            'favorites' => $request->input('favorites', array('なし')),
+    //            'memo' => $memo
+    //        );
+    //        return view('profile.show', $profile);
         
     }
 }
