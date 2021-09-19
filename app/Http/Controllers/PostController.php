e1a4bed86892a1ce3756b4a5178fba155fe897d1
morikawa0170 <mori7replicant10@gmail.com>
2021-09-19 10:07:16 +0900

    詳細ページ表示機能追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -58,7 +58,9 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        //
+        $posts = Post::find($id);
+
+        return view('posts.show', compact('posts'));
     }
 
     /**
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -33,7 +33,6 @@
 
    .RightJustified{
    float: right;
-   padding-right: 550px;
    font-size: 12px;
    margin-left: 20px;
    }
@@ -55,91 +54,113 @@
    footer{
    text-align: center;
    }
+
+   .good {
+      background-color: rgb(0, 199, 221);
+      height: 20px;
+      width: 80px;
+      color: #fff;
+      font-weight: bold;
+      border: none;
+      cursor:pointer;
+   }
+
+   .goodyet {
+      background-color: #fff;
+      height: 20px;
+      width: 80px;
+      color: rgb(0, 199, 221);
+      font-weight: bold;
+      border: 1px solid rgb(0, 199, 221);
+      cursor:pointer;
+   }
 @endsection
 
 @section('content')
 <main>
-   <br>
-   <br>
-   <br>
-   <br>
-   <br>
-   <br>
-   <br>
-   <br>
-   <h2>検索結果</h2>
-   <hr>
-   <br>
-   <br>
-   <h3>最新の記事</h3>
-   <hr>
-   <img src="/images/profileimage.png" alt="プロフィール画像" height="150px" class="mb-3">
-   <h3>投稿タイトル：***</h3>
-   <p>ユーザー名</p>
-   <p>記事の内容：</p>
-   <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
-   <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
-   <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
-   <br>
-   <h4>[基本データ]</h4>
-   <p>　　記事の種類：</p>
-   <p>  都道府県</p>
-   <p>活動日：</p>
-   <p>バンド楽曲：</p>
-   <p>[募集メンバー]　性別/年齢</p>
-   <p>[ジャンル]</p>
-   <p>[募集パート]</p>
-   <p>[コンセプト]社会人バンド</p>
-   <div class="RightJustified">
-      <p>投稿日時：00:00</p>
-      <a href="SearchResultdetail1.html"><img src="image/iine0.png" alt="いいねボタン" class="image-cng" height="20px"></a>
+   <div class="container">
+      <div style="font-size: 25px;">記事詳細</div>
+      <hr>
+      <!-- <img src="/images/profileimage.png" alt="プロフィール画像" height="150px" class="mb-3"> -->
+      <div>
+         <h3 class="mb-3">投稿タイトル：{{ $posts->title }}</h3>
+
+      </div>
+      <p class=>投稿者 :  {{ $posts->user->name }}</p>
+      <p>記事の内容：</p>
+      <p>{{ $posts->message }}</p>
+      <br>
+      <h5 class="my-3">[基本データ]</h5>
+      <p>活動地域 : {{ $posts->activity_area }}</p>
+      <p>活動レベル : {{ $posts->activity_level }}</p>
+      <p>バンドレベル : {{ $posts->band_level }}</p>
+      <p>ジャンル : {{ $posts->genre }}</p>
+      <p>好きなアーティスト : {{ $posts->favorite_artist }}</p>
+      <p>活動時間帯： {{$posts->activity_timezone}}</p>
+      <h5 class="mb-3 mt-4">[募集メンバー]</h5>
+      <p>[募集パート] : {{ $posts->recruitment_part }}</p>
+      <p>[求めるレベル感] : {{ $posts->required_level }}</p>
+      <p>[性別] : {{ $posts->sex }} </p>
+      <p>[年齢] : {{ $posts->age }}代</p>
+      <div class="RightJustified">
+         <p>投稿日時： {{ $posts->created_at }}</p>
+         
+         <form action="">
+            <button type="submit" class="good">いいね</button>
+            <span class="ml-2 border-left-0" style="line-height: 20px; font-size: 14px;"></span>
+         </form>
+         <form action="">
+            <button type="submit" class="goodyet">いいね</button>
+            <span class="ml-2 border-left-0" style="line-height: 20px; font-size: 14px;"></span>
+         </form>
+         <br>
+         <br>
+      </div>
       <br>
       <br>
-   </div>
-   <br>
-   <br>
-   <div class="article"><hr></div>
-   <!-- <h3>投稿タイトル2</h3>
-   <div class="toukou">
-      <p>ユーザー名(地域 年齢)</p>
-      <p>本文最初の数行を表示</p>
-      <p>投稿日時</p>
-      <br>
-      <br>
-   </div>
-   <hr>
-   <h3>投稿タイトル3</h3>
-   <div class="toukou">
-      <p>ユーザー名(地域 年齢)</p>
-      <p>本文最初の数行を表示</p>
-      <p>投稿日時</p>
-      <br>
-      <br>
+      <div class="article mb-5"><hr></div>
+      <!-- <h3>投稿タイトル2</h3>
+      <div class="toukou">
+         <p>ユーザー名(地域 年齢)</p>
+         <p>本文最初の数行を表示</p>
+         <p>投稿日時</p>
+         <br>
+         <br>
       </div>
       <hr>
-      <h3>投稿タイトル4</h3>
+      <h3>投稿タイトル3</h3>
       <div class="toukou">
-      <p>ユーザー名(地域 年齢)</p>
-      <p>本文最初の数行を表示</p>
-      <p>投稿日時</p>
+         <p>ユーザー名(地域 年齢)</p>
+         <p>本文最初の数行を表示</p>
+         <p>投稿日時</p>
+         <br>
+         <br>
+         </div>
+         <hr>
+         <h3>投稿タイトル4</h3>
+         <div class="toukou">
+         <p>ユーザー名(地域 年齢)</p>
+         <p>本文最初の数行を表示</p>
+         <p>投稿日時</p>
+         <br>
+         <br>
+      </div>
       <br>
       <br>
-   </div>
-   <br>
-   <br>
-   <br>
-   <br>
-   <br>
-   <br>
+      <br>
+      <br>
+      <br>
+      <br>
 
-   <br>
-   <br>
-   <br>
-   <br>
-   <br>
-   <br>
-   <br>
-   <br>
-   <br> -->
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br> -->
+   </div>
 </main>
  @endsection
